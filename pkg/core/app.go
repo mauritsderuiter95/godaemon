dbb3724d6d197ac4bc4aad11acfae9f6d46bde6c
Maurits de Ruiter <mauritsderuiter95@gmail.com>
2021-09-26 21:25:11 +0200

    Removed redundant methods

--- pkg/core/app.go
+++ pkg/core/app.go
@@ -1,19 +1,5 @@
 package core
 
-import "time"
-
 type UserApp interface {
 	Initialize()
-}
-
-type App struct {
-
-}
-
-func (a *App) ListenState(f func(), id string) {
-
-}
-
-func (a *App) RunDaily(f func(), at time.Time) {
-
 }
