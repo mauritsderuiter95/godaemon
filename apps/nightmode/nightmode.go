98b4de70bb2af7b25cd4acfb9279aecd35392004
Maurits de Ruiter <mauritsderuiter95@gmail.com>
2021-09-27 21:49:58 +0200

    Added logging

--- apps/nightmode/nightmode.go
+++ apps/nightmode/nightmode.go
@@ -1,7 +1,6 @@
 package nightmode
 
 import (
-	"fmt"
 	"github.com/mauritsderuiter95/godaemon/pkg/core"
 )
 
@@ -9,12 +8,6 @@ type Nightmode struct {
 }
 
 func (n Nightmode) Initialize() {
-	state, err := core.State{EntityId: "light.woonkamer"}.Get()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("%#v\n", state)
 	core.RunEveryDay(22, 22, n.TurnOffLivingRoom)
 	core.Event{EventType: "deconz_event"}.On("switch_woonkamer", n.ToggleKitchen)
 }
--- cmd/godaemon/main.go
+++ cmd/godaemon/main.go
@@ -4,21 +4,27 @@ import (
 	"github.com/mauritsderuiter95/godaemon/apps"
 	"github.com/mauritsderuiter95/godaemon/pkg/core"
 	"log"
+	"reflect"
 )
 
 func main() {
+	log.Println("Starting GoDaemon")
 	inst := core.GetInstance()
+	go core.QuitOnFilechange(inst)
 
+	log.Println("Registering apps")
 	userApps := apps.Register()
 
 	for _, app := range userApps {
 		app.Initialize()
+		log.Println("Registered", reflect.TypeOf(app).Name())
 	}
 
 	go inst.HandleEvents()
+	log.Println("Listening to events")
 	core.StartScheduler()
-
-	go core.QuitOnFilechange(inst)
+	log.Println("Started scheduler")
+	log.Println("Ready")
 
 	if err := inst.CloseConnection(); err != nil {
 		log.Fatalln(err)
